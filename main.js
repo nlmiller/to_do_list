e4253b0c22701bdfd47b3d63a670f6b06bd0f2ff
Nicole Miller <nlmiller137@gmail.com>
2013-06-25 15:13:18 -0400

    third commit

--- main.js
+++ main.js
@@ -1,6 +1,71 @@
 // JavaScript Document
 
-var toDo = ['Homework', 'Gym', 'Cook Dinner', 'Shower', 'Laundry'];
+var homework = {
+    "name": "Homework",
+    status: "incomplete",
+    date: "Due 6/28/13"
+};
+
+var gym = {
+    "name": "Gym",
+    status: "complete",
+    date: "Due 6/18/13"
+};
+
+var cookDinner = {
+    "name": "Cook Dinner",
+    status: "complete",
+    date: "Due 6/18/13"
+};
+
+var shower = {
+    "name": "Shower",
+    status: "complete",
+    date: "Due 6/18/13"
+};
+
+var laundry = {
+    "name": "Laundry",
+    status: "complete",
+    date: "Due 6/18/13"
+};
+
+var name;
+for (name in homework) {
+    if (typeof homework[name] !== 'function')
+    document.write(homework[name]);
+}
+
+for (name in gym) {
+    if (typeof gym[name] !== 'function')
+    document.write(gym[name]);
+}
+
+for (name in cookDinner) {
+    if (typeof cookDinner[name] !== 'function')
+    document.write(cookDinner[name]);
+}
+
+for (name in shower) {
+    if (typeof shower[name] !== 'function')
+    document.write(shower[name]);
+}
+
+for (name in laundry) {
+    if (typeof laundry[name] !== 'function')
+    document.write(laundry[name]);
+}
+
+
+function finished(status) {
+  if (status == 'complete')
+    return status;
+  else
+  throw "Task is not yet completed";
+}
+
+/*
+var toDo = [ homework.name, gym.name, cookDinner.name , shower.name, laundry.name];
 
 document.write('<ul>'); //print unordered list onto html page
 
@@ -9,3 +74,7 @@ for(var i = 0; i < toDo.length; i++) {
 }
 
 document.write('</ul>');  //end unordered list
+*/
+
+
+
